f8f6aea7c01085b298849c6b1fddad8e7b8732f7
Mohin Uddin <mdmohinuddin4721@gmail.com>
2025-04-21 16:34:49 +0600

    added console

--- frontend/src/redux/userAPI.js
+++ frontend/src/redux/userAPI.js
@@ -9,6 +9,7 @@ export const getUserAPI = async () => {
         },
     });
     const data = await response.json();
+    console.log("user api data ", data);
     return data;
 }
 export const signupAPI = async (userData) => {
